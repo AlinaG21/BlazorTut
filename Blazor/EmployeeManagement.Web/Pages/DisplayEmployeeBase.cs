cf7f03cff15c3e30f79ee81b2f0b58371cd98ded
DESKTOP-KR8JJH5\alinag <lina.gosteva@gmail.com>
2021-07-03 01:08:07 +0300

    19 video

---
+++ Blazor/EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DisplayEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public IEnumerable<Employee> Employees { get; set; }
+
+        public bool ShowFooter { get; set; } = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employees = (await EmployeeService.GetEmployees()).ToList();
+        }
+    }
+}
