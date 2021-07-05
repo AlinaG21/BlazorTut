264fdf8a2f936cad6867698d46651bb5388150d0
DESKTOP-KR8JJH5\alinag <lina.gosteva@gmail.com>
2021-07-05 23:46:50 +0300

    27 video

--- Blazor/EmployeeManagement.Models/Employee.cs
+++ Blazor/EmployeeManagement.Models/Employee.cs
@@ -10,8 +10,8 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
-        [Required]
-        [StringLength(100, MinimumLength = 2)]
+        [Required(ErrorMessage = "FirstName is mandatory")]
+        [MinLength(2)]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
