844e95188a9aa75c1c4bbd709841a5e1cad21f7d
DESKTOP-KR8JJH5\alinag <lina.gosteva@gmail.com>
2021-07-07 12:11:55 +0300

    36 video

--- Blazor/EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
+++ Blazor/EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -10,11 +10,39 @@ namespace EmployeeManagement.Web.Pages
 {
     public class DisplayEmployeeBase : ComponentBase
     {
+        [Parameter]
+        public EventCallback<int> OnEmployeeDeleted { get; set; }
+
+        private IEmployeeService employeeService;
+
+        public IEmployeeService GetEmployeeService()
+        {
+            return employeeService;
+        }
+
+        public void SetEmployeeService(IEmployeeService value)
+        {
+            employeeService = value;
+        }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected async Task Delete_Click()
+        {
+            await GetEmployeeService().DeleteEmployee(Employee.EmployeeId);
+            await OnEmployeeDeleted.InvokeAsync(Employee.EmployeeId);
+            //NavigationManager.NavigateTo("/", true);
+        }
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
         public IEnumerable<Employee> Employees { get; set; }
 
+        [Parameter]
+        public Employee Employee { get; set; }
+
+        [Parameter]
         public bool ShowFooter { get; set; } = true;
         
         public EventCallback<bool> OnEmployeeSelection { get; set; }
